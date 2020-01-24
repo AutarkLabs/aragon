2d38312b0fb99b377212cf42ec29879879d4a543
PeterMPhillips <github@pmphillips.ca>
2020-01-24 15:02:39 -0800

    feat: storing org info in local storage to reduce load time

--- src/contexts/OrgInfoContext.js
+++ src/contexts/OrgInfoContext.js
@@ -1,10 +1,12 @@
 import React, { createContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useClientTheme } from '../client-theme'
+import { setClientOrgInfo, getClientOrgInfo } from '../local-settings'
 import { useOrganizationDataStore } from '../hooks'
 
 const ORG_SETTINGS_BASIC_INFO = 'ORG_SETTINGS_BASIC_INFO'
 const ORG_SETTINGS_BRAND = 'ORG_SETTINGS_BRAND'
+const CLIENT_ORG_INFO = getClientOrgInfo()
 
 export const OrgInfoContext = createContext({})
 
@@ -15,7 +17,7 @@ export const OrgInfoProvider = ({children}) => {
     ipfsProviderConnectionSuccess,
   } = useOrganizationDataStore()
   const { appearance, updateClientTheme } = useClientTheme()
-  const [orgInfo, setOrgInfo] = useState()
+  const [orgInfo, setOrgInfo] = useState(CLIENT_ORG_INFO)
   const [fetchedData, setFetchedData] = useState(false)
 
   const fetchOrgInfo = async () => {
@@ -31,14 +33,18 @@ export const OrgInfoProvider = ({children}) => {
         data.theme = style.theme
       }
     }
+    const clientData = {...data} //images stored in local storage will differ than stored in state, thus two objects
     if (logo_cid) {
       const logo = await ipfsEndpoints.cat(logo_cid)
       if (logo && logo.ok) {
         const arrayBuffer = await logo.arrayBuffer()
         data.image = URL.createObjectURL(new Blob([arrayBuffer], { type: "image/jpeg" } ))
+        clientData.image = JSON.stringify(Array.from(new Uint32Array(arrayBuffer)))
       }
     }
+
     setOrgInfo(data)
+    setClientOrgInfo(clientData)
     setFetchedData(true)
 
     updateClientTheme(
