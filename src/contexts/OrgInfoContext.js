958ce7c71e191503d438792d0068c700aa0e98c9
Peter Phillips <github@pmphillips.ca>
2020-01-31 16:50:57 -0500

    fix: remove caching until problem solved

--- src/contexts/OrgInfoContext.js
+++ src/contexts/OrgInfoContext.js
@@ -25,18 +25,17 @@ export const OrgInfoProvider = ({
     ipfsProviderConnectionSuccess,
   } = useOrganizationDataStore()
   const { appearance, updateClientTheme } = useClientTheme()
-  const [orgInfo, setOrgInfo] = useState(CLIENT_ORG_INFO)
-  //const [fetchedData, setFetchedData] = useState(false)
+  const [orgInfo, setOrgInfo] = useState()
 
   const getThisOrg = useCallback(() => {
     if (!dao || !orgInfo) return null
     return orgInfo[dao]
-  }, [dao])
+  }, [dao, orgInfo])
 
   const getOrg = useCallback((dao) => {
     if (!dao || !orgInfo) return null
     return orgInfo[dao]
-  })
+  }, [dao, orgInfo])
 
   const fetchOrgInfo = useCallback(async () => {
     const [
@@ -54,7 +53,7 @@ export const OrgInfoProvider = ({
         data.theme = style.theme
       }
     }
-    const clientData = { ...data } // images stored in local storage will differ than stored in state, thus two objects
+    //const clientData = { ...data } // images stored in local storage will differ than stored in state, thus two objects
     if (logoCid) {
       const logo = await ipfsEndpoints.cat(logoCid)
       if (logo && logo.ok) {
@@ -62,17 +61,20 @@ export const OrgInfoProvider = ({
         data.image = URL.createObjectURL(
           new Blob([arrayBuffer], { type: 'image/jpeg' })
         )
+        /*
         clientData.image = JSON.stringify(
           Array.from(new Uint32Array(arrayBuffer))
         )
+        */
       }
     }
+
     const newOrgInfo = {...orgInfo}
-    const newClientOrgInfo = {...CLIENT_ORG_INFO}
+    //const newClientOrgInfo = {...CLIENT_ORG_INFO}
     newOrgInfo[dao] = data
-    newClientOrgInfo[dao] = clientData
+    //newClientOrgInfo[dao] = clientData
     setOrgInfo(newOrgInfo)
-    setClientOrgInfo(newClientOrgInfo)
+    //setClientOrgInfo(newClientOrgInfo)
     setFetchedData(true)
 
     updateClientTheme(appearance, {
